3f0e1c5eb0c0d2823afd8118980adb1b138fb656
NavaUDP <diego.navarrete2@mail.udp.cl>
2024-05-28 17:16:34 -0400

    solicitud funciona correctamente y se comunica con procedure

--- solicitud.py
+++ solicitud.py
@@ -1,25 +1,48 @@
-import subprocess
+from confluent_kafka import Producer
 import json
 import os
 
-#obtener el producto
-with open('data/datos.json', 'r') as file:
-    data = json.load(file)
+#Configuracion producer
+conf = {'bootstrap.servers': 'localhost:9092'}
 
-# si ya existe el archivo datos_con_ids.json existe, se ejecuta el producer y se termina el script
-if os.path.exists('data/datos_con_ids.json'):
-    print("El archivo datos_con_ids.json ya existe.")
-    subprocess.run(["python3", "API/producer.py"])
-    exit()
-else:
-    productos = data['productos']
-    for i, producto in enumerate(productos):
-        producto['id'] = i + 1
+#creamos al productor
+producer = Producer(conf)
 
-    # Escribir los productos con sus identificadores en un nuevo archivo JSON
-    with open('data/datos_con_ids.json', 'w') as file:
-        json.dump(productos, file)
-    print("Productos guardados con id.")
+# Función de callback para saber si un mensaje fue entregado o no
+def delivery_report(err, msg):
+    if err is not None:
+        print(f'Mensaje falló la entrega: {err}')
+    else:
+        print(f'Mensaje entregado a {msg.topic()} [{msg.partition()}]')
 
-    #ejecutar producer.py
-    subprocess.run(["python3", "API/producer.py"])
+fin = 0
+while fin == 0:
+    #Funcion para saber si se agregaron las id a los productos
+    if os.path.isfile('data/datos_con_ids.json'):
+        #en caso de existir el archivo entonces se envía el mensaje a kafka
+        with open('data/datos_con_ids.json', 'r') as file:
+            data = json.load(file)
+    
+        for item in data:
+            #Convertir el producto a una cadena JSON
+            item_str = json.dumps(item)
+
+            #Enviar el producto al topic
+            producer.produce('test', key = 'key', value=item_str, callback=delivery_report)
+
+            producer.flush()
+        print("Productos enviados a Kafka.")
+        fin = 1
+    else:
+        with open('data/datos.json', 'r') as file:
+            data = json.load(file)
+
+        productos = data['productos']
+        for i, producto in enumerate(productos):
+            producto['id'] = i + 1
+
+            # Escribir los productos con sus identificadores en un nuevo archivo JSON
+            with open('data/datos_con_ids.json', 'w') as file:
+                json.dump(productos, file)
+    
+        print("Productos guardados con id.")
