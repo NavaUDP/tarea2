8c1ef68369dbc7311aec4e91dce529998ae0fd08
NavaUDP <diego.navarrete2@mail.udp.cl>
2024-05-29 04:35:52 -0400

    cambios realizados en procedure, ahora lee los mensajes del canal y cambia los estados de los productos

--- procedure.py
+++ procedure.py
@@ -1,51 +1,54 @@
-from confluent_kafka import Consumer, KafkaException
+from confluent_kafka import Consumer, Producer, KafkaException
 import json
 
-# Configuración del consumidor
-conf = {
-    'bootstrap.servers': 'localhost:9092',
-    'group.id': 'mygroup',
-    'auto.offset.reset': 'earliest',
-}
+#Configuraciones Consumer y Producer
+conf_consumer = {'bootstrap.servers': 'localhost', 'group.id': 'mygroup', 'auto.offset.reset': 'earliest'}
+conf_producer = {'bootstrap.servers': 'localhost:9092'}
 
-# Crear el consumidor con la configuración dada
-consumer = Consumer(conf)
+#crear al consumer y producer
+consumer = Consumer(conf_consumer)
+producer = Producer(conf_producer)
 
-# Suscribirse al topic 'test'
+#Entramos al topic
 consumer.subscribe(['test'])
+print("Entrando al topic test")
 
 try:
     while True:
-        # Leer mensaje del topic
-        msg = consumer.poll(1.0)
-
-        if msg is None:
+        #lee el mensaje
+        message = consumer.poll(1.0)
+        if message is None:
             continue
-        if msg.error():
-            raise KafkaException(msg.error())
+        if message.error():
+            raise KafkaException(message.error())
         else:
-            # Decodificar mensaje y cargarlo como un diccionario
-            msg_dict = json.loads(msg.value().decode("utf-8"))
+            #Decodifica el mensaje
+            msg_dict = json.loads(message.value().decode('utf-8'))
 
-            # Verificar y cambiar el estado
+            #Si el estado es finalizado, se ignora el mensaje
+            if 'estado' in msg_dict and msg_dict['estado'] == 'finalizado':
+                continue
+
+            print(f"Mensaje recibido: {msg_dict}")
+
+            #Verificamos que el mensaje tenga un estado
             if 'estado' not in msg_dict:
+                #Asigna uno
                 msg_dict['estado'] = 'recibido'
-            elif msg_dict['estado'] == 'recibido':
-                msg_dict['estado'] = 'preparando'
-            elif msg_dict['estado'] == 'preparando':
-                msg_dict['estado'] = 'entregando'
-            elif msg_dict['estado'] == 'entregando':
-                msg_dict['estado'] = 'finalizado'
+            else:
+                #cambia al siguiente estado
+                if msg_dict['estado'] == 'recibido':
+                    msg_dict['estado'] = 'preparando'
+                elif msg_dict['estado'] == 'preparando':
+                    msg_dict['estado'] = 'entregado'
+                elif msg_dict['estado'] == 'entregado':
+                    msg_dict['estado'] = 'finalizado'
 
-            # Guardar el diccionario actualizado en el archivo 'data_con_parametros.json'
-            with open('data/datos_con_estado.json', 'a') as file:
-                json.dump(msg_dict, file)
-                file.write('\n')
+            #Envia el mensaje de vuelta al topic
+            producer.produce('test', json.dumps(msg_dict).encode('utf-8'))
 
-            # Imprimir mensaje
-            print(f'Mensaje recibido: {json.dumps(msg_dict)}')
 except KeyboardInterrupt:
     pass
 finally:
-    # Cerrar el consumidor
     consumer.close()
+    producer.flush()
