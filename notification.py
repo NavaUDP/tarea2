c1dbc4d3387eec115a14aaf6efb0892d9ae6a711
NavaUDP <diego.navarrete2@mail.udp.cl>
2024-06-03 03:59:03 -0400

    push final

--- notification.py
+++ notification.py
@@ -31,70 +31,10 @@ def get_status(order_id):
         return jsonify({"error": "Pedido no encontrado"}), 404
 
  #Configuracion para realizar la peticion smtp
-
-
-HOST = "smtp-mail.outlook.com"
+HOST = "smtp.gmail.com"
 PORT = 587
 smtp_username = os.environ.get('API envíos', 'dnavarretev26@gmail.com')
-smtp_password = os.environ.get('API envíos', 'fcpd tevo mbmf ilrj') 
-
-
-def process_message(msg_dict):
-    order_id = msg_dict['id-envio']
-    if order_id is None:
-        logging.error(f"No se encontró el ID en el mensaje: {msg_dict}")
-        return
-        
-    last_status[order_id] = msg_dict
-
-    # Verificar si el mensaje tiene un correo electrónico
-    if 'correo' in msg_dict:
-        to_email = msg_dict['correo']
-        subject = f"Actualización de tu pedido {order_id}"
-        body = create_email_body(msg_dict)
-        send_email(to_email, subject, body)
-    else:
-        logging.warning(f"No se encontró campo 'correo' en el mensaje: {msg_dict}")
-
-    print(f"Mensaje recibido y procesado: {msg_dict}")
-
-def send_email(to_email, subject, body):
-    msg = MIMEMultipart()
-    msg['From'] = smtp_username
-    msg['To'] = to_email
-    msg['Subject'] = subject
-    msg.attach(MIMEText(body, 'plain'))
-
-    try:
-        with smtplib.SMTP(HOST, PORT) as server:
-            server.starttls()
-            server.login(smtp_username, smtp_password)
-            server.send_message(msg)
-            logging.info(f"Correo enviado a {to_email}")
-    except smtplib.SMTPAuthenticationError:
-        logging.error("Error de autenticación SMTP. Verifica tus credenciales.")
-    except smtplib.SMTPException as e:
-        logging.error(f"Error SMTP: {e}")
-    except Exception as e:
-        logging.error(f"Error inesperado al enviar el correo a {to_email}: {e}")
-
-def create_email_body(msg_dict):
-    order_id = msg_dict.get('id-envio')
-    estado = msg_dict.get('estado', 'En proceso')
-
-    return f"""
-    ¡Hola! Gracias por usar nuestro servicio de delivery.
-
-    (ID: {order_id}) ha sido actualizado.
-
-    - Estado actual: {estado}
-
-
-    Te mantendremos informado sobre cualquier cambio en tu pedido.
-    ¡Que tengas un buen día!
-
-    El equipo de Delivery UDM
-    """
+smtp_password = os.environ.get('vsCode', 'akxj napd sref fees') 
 
 try:
     # Iniciar Flask en un hilo separado
@@ -116,7 +56,59 @@ try:
             if 'estado' not in msg_dict or msg_dict['estado'] != 'finalizado':
                 continue
 
-            print(f"Mensaje recibido: {msg_dict}")
+
+            order_id = msg_dict['id-envio']
+            last_status[order_id] = msg_dict
+
+            def create_email_body(msg_dict):
+                order_id = msg_dict.get('id-envio')
+                estado = msg_dict.get('estado', 'En proceso')
+
+                return f"""
+                ¡Hola! Gracias por usar nuestro servicio de delivery.
+
+                (ID: {order_id}) ha sido actualizado.
+
+                - Estado actual: {estado}
+
+
+                Te mantendremos informado sobre cualquier cambio en tu pedido.
+                ¡Que tengas un buen día!
+
+                El equipo de Delivery UDM
+                """
+
+            def send_email(to_email, subject, body):
+                msg = MIMEMultipart()
+                msg['From'] = smtp_username
+                msg['To'] = to_email
+                msg['Subject'] = subject
+                msg.attach(MIMEText(body, 'plain'))
+
+                try:
+                    with smtplib.SMTP(HOST, PORT) as server:
+                        server.starttls()
+                        server.login(smtp_username, smtp_password)
+                        server.send_message(msg)
+                        logging.info(f"Correo enviado a {to_email}")
+                except smtplib.SMTPAuthenticationError:
+                    logging.error("Error de autenticación SMTP. Verifica tus credenciales.")
+                except smtplib.SMTPException as e:
+                    logging.error(f"Error SMTP: {e}")
+                except Exception as e:
+                    logging.error(f"Error inesperado al enviar el correo a {to_email}: {e}")
+
+            # Verificar si el mensaje tiene un correo electrónico
+            if 'email' in msg_dict:
+                to_email = msg_dict['email']
+                subject = f"Actualización de tu pedido {order_id}"
+                body = create_email_body(msg_dict)
+                send_email(to_email, subject, body)
+            else:
+                logging.warning(f"No se encontró campo 'correo' en el mensaje: {msg_dict}")
+
+            print(f"Mensaje recibido y procesado: {msg_dict}")
+
 
 except KeyboardInterrupt:
     pass
